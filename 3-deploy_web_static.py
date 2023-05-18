b49d90047c65822a824b328747120e86a2134dca
Adexlexy <oshewolo_martins@yahoo.com>
2023-05-18 03:14:55 -0700

    Airbnb clone Deploy static

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Fabfile to create and distribute an archive to a web server.
+import os.path
+from datetime import datetime
+from fabric.api import env
+from fabric.api import local
+from fabric.api import put
+from fabric.api import run
